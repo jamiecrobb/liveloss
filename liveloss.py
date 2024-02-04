4caf87c78a2fb92394ccf1610b5169784d0860f3
Jamie Robb <jamierobb321@gmail.com>
2024-02-04 16:14:26 +0000

    Working with example usage in model.ipynb

--- liveloss.py
+++ liveloss.py
@@ -1,4 +1,29 @@
 import matplotlib.pyplot as plt
+from IPython.display import display, clear_output
+import numpy as np
+
 
 class LiveLoss:
-    
+    def __init__(self):
+        self.fig, self.ax = plt.subplots()
+        self.line, = self.ax.plot([], 'o-')
+        self.xs, self.ys = [], []
+        self.line.set_data(self.xs, self.ys)
+        self.ax.relim()
+        self.ax.autoscale_view()
+
+    def update(self):
+        self.line.set_data(self.xs, self.ys)
+        self.ax.relim()
+        self.ax.autoscale_view()
+        clear_output(wait=True)
+        display(self.fig)
+
+    def send(self, x, y):
+        self.xs.append(x)
+        self.ys.append(y)
+        self.update()
+        plt.pause(0.1)
+
+    def get_line(self):
+        return self.line,
